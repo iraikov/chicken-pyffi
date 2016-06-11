d244942f99dab88979eb10e3f67bd6b86a03ea11
Ivan Raikov <ivan.g.raikov@gmail.com>
2016-06-11 15:10:48 -0700

    bug fixes in defining scheme names for python methods and slots; updated h5py example

--- examples/h5py.scm
+++ examples/h5py.scm
@@ -26,19 +26,19 @@
 
 
 (define-pyfun "h5py.File" name mode)
-(define-pymethod "close") ;; File
-(define-pymethod "create_dataset" kw: (dtype)) ;; File
+(define-pymethod "close" scheme-name: File.close) ;; File
+(define-pymethod "create_dataset" scheme-name: File.create_dataset kw: (dtype)) ;; File
 
-(define-pyslot "shape")
-(define-pyslot "dtype")
-(define-pyslot "name")
+(define-pyslot "shape" Dataset.shape)
+(define-pyslot "dtype" Dataset.dtype)
+(define-pyslot "name" Dataset.name)
 
 (define f (h5py.File "mytestfile.hdf5" "w"))
 
-(define dset (create_dataset f "mydataset"  (vector 100) dtype: "i"))
+(define dset (File.create_dataset f "mydataset"  (vector 100) dtype: "i"))
 
-(print (py-object-from (name dset)))
-(print (py-object-from (shape dset)))
-(print (py-object-type (dtype dset)))
+(print (py-object-from (Dataset.name dset)))
+(print (py-object-from (Dataset.shape dset)))
+(print (py-object-type (Dataset.dtype dset)))
 
-(close f)
+(File.close f)
